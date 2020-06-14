6046a40d95b13b5859f2b2217712d290dba05c1a
nova zaky <novazaky1@gmail.com>
2020-06-14 18:03:11 +0700

    feature-save-history-user-balance

--- src/model/userBalanceHistoryModel.js
+++ src/model/userBalanceHistoryModel.js
@@ -6,6 +6,24 @@ class userBalanceHistoryModel extends Models{
         super(user_balance_history)
     }
 
+    async saveHistory(obj){
+        const {user_balance_id, balance_before, balance_after, activity, tipe, ip, location, user_agent, author}=obj
+        var history = await this.model.create({
+            user_balance_id,
+            balance_before,
+            balance_after,
+            activity,
+            tipe,
+            ip,
+            location,
+            user_agent,
+            author
+        })
+        // console.log({history});
+        
+        return history
+    }
+
 }
 
 module.exports=userBalanceHistoryModel
