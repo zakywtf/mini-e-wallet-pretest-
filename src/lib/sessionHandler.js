e90a086caa01cb578e9ce04ae32e0af0fe8c4e50
nova zaky <novazaky1@gmail.com>
2020-06-14 16:02:43 +0700

    feature-session-handler

---
+++ src/lib/sessionHandler.js
@@ -0,0 +1,55 @@
+import jwt from 'jsonwebtoken'
+import moment from 'moment';
+
+const SESSION = []
+
+const decode = async(token) => {
+    return jwt.decode(token, {complete: true})
+}
+
+const idleTime = async() => {
+    return parseInt(process.env.SESSION_TIME) * 60 * 1000 //menit
+}
+
+const createSession = async(token) => {
+    var dc = await decode(token)
+    var time = await idleTime()
+
+    // console.log({dc});
+    var udata = dc.payload.payload
+    var cekSesi = await checkSession(udata)
+    if(cekSesi == true){
+        var datas = {
+            udata:udata,
+            session:true,
+            isOnline:true,
+            lastOnline:moment().format('YYYY-MM-DD HH:mm:ss')
+        }
+        setTimeout(()=>{
+            deleteSession(udata)
+        },time)  
+        SESSION[`${udata.id}`]=datas
+        // console.log({token, userId});
+        console.log(SESSION, 'on addsession');
+
+        return token
+    }else{
+        // return {error:401, message:`Anda sedanng login di device lain!`}
+        throw new Error('Anda sedang login di user lain!')
+    }
+}
+
+const checkSession = async(udata) => {
+    var sessionData = SESSION[`${udata.id}`]
+    console.log({sessionData});
+    if(sessionData){
+        return (sessionData.session === true)?false:true
+    }else{
+        return true
+    }
+}
+
+module.exports = {
+    createSession,
+    decode
+}
